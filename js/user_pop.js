e603c5007e6f1dc91ae66463340b66b0d2c4cea9
THE UNFORGOTTEN <132151773+YousifMagdy@users.noreply.github.com>
2024-12-07 22:22:52 +0200

    Add files via upload

---
+++ js/user_pop.js
@@ -0,0 +1,53 @@
+function User_add(){
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+    toastr["info"]("User added successfully", "ADD User")
+}
+
+function User_del(){
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+        toastr["DELETE"]("User deleted successfully", "Delete User")
+    }
+    
+
+
+
+function Delete_User(id){
+    let del = confirm("هتمسحه بجد ؟");
+    console.log(del)
+    if(del == true){
+        window.location.href="index.php?action=del&&id"+id;
+    }
+}
+
